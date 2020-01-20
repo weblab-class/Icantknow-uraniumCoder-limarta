f825d9d8fe7534a797c2bfa6e1c3f44b941cc566
limarta <ian@DESKTOP-MFGREKE.localdomain>
2020-01-20 15:19:25 -0500

    Find Game pages skeleton

--- client/src/components/pages/FindGames.js
+++ client/src/components/pages/FindGames.js
@@ -6,15 +6,23 @@ import {get} from "../../utilities.js";
 class FindGames extends Component{
   constructor(props){
     super(props);
+    this.state = {
+        games : []
+    }
   }
   componentDidMount(){
-
+        get("/api/games").then((games)=>this.setState({games:games}))
   }
   render(){
-    return (
-      <>
-      </>
-    );
+    let gameBlock = "No available games"
+    if(games.length!==0){
+        gameBlock = this.state.games.map((game)=><FindGameBlock _id={game.id} content={game.content}/>)
+    }
+
+    return (<div>
+        {}
+        {gameBlock}
+    </div>);
   }
 }
 
